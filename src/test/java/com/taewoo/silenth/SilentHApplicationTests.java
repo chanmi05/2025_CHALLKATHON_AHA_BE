e6b889bf92529dff523c033c156952d8bb6176d7
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-06-21 21:56:14 +0900

    Silent Hour Initial Commit

---
+++ src/test/java/com/taewoo/silenth/SilentHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taewoo.silenth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilentHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
