dd0ee3cc2156d77adb0fc77d98b0b109ee1ff32f
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-06-25 03:46:48 +0900

    mypage & error

--- src/main/java/com/taewoo/silenth/config/UserPrincipal.java
+++ src/main/java/com/taewoo/silenth/config/UserPrincipal.java
@@ -40,7 +40,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return user.getLoginId();
     }
 
     @Override
